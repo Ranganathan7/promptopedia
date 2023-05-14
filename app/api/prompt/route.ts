a267f0eb150b52b3dc35d935014605bd59cc56ce
Ranganathan S V <rangu230801@gmail.com>
2023-05-14 14:36:50 +0530

    updated /api/prompt to not cache data

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,11 +1,16 @@
 import { connectToDb } from "../../../utils/database.connection";
 import PromptModel from "../../../models/prompt.model";
 import { NextRequest } from "next/server";
+import { NextApiResponse } from "next";
 
-export const GET = async (req: NextRequest) => {
+export const GET = async (req: NextRequest, res: NextApiResponse) => {
   try {
     await connectToDb();
     const prompts = await PromptModel.find({}).populate("userId");
+    res.setHeader(
+      "Cache-Control",
+      "no-cache, no-store, max-age=0, must-revalidate"
+    );
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (err) {
     return new Response(JSON.stringify("Failed to fetch prompts!"), {
